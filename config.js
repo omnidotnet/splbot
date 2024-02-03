93c6722b6ed20e3e09c465baca963625bf9adc32
Yudjenn <yudjenrb@icloud.com>
2024-02-03 18:27:15 +0000

    rah

--- config.js
+++ config.js
@@ -10,27 +10,27 @@ let config = {
         geo: {'code': 'gb', 'lat': 50, 'lon': 0},
         roomScript: './room.js'
     },
-    // publicdiv1: {
-    //     autoStart: true,
-    //     roomName: '🟣⚫⚽️ 𝗦.𝗣.𝗟. 1 🏆 | Futsal 4v4 ⚽️⚫🟣',
-    //     playerName: 'host',
-    //     maxPlayers: 30,
-    //     public: false,
-    //     noPlayer: true,
-    //     token: 'thr1.AAAAAGW9P338BLvRrJ5OCQ.L46X3hVjHnA',
-    //     geo: {'code': 'gb', 'lat': 50, 'lon': 0},
-    //     roomScript: './pub.js'
-    // },
-    // publicdiv2: {
-    //     autoStart: true,
-    //     roomName: '🟣⚫⚽️ 𝗦.𝗣.𝗟. 2 🔥 | Futsal 4v4 ⚽️⚫🟣',
-    //     playerName: 'host',
-    //     maxPlayers: 30,
-    //     public: false,
-    //     noPlayer: true,
-    //     token: 'thr1.AAAAAGWYV2uVDc3nNi-SDg.P2QFF44FfRc',
-    //     geo: {'code': 'gb', 'lat': 50, 'lon': 0},
-    //     roomScript: './pub.js'
-    // }
+    publicdiv1: {
+        autoStart: true,
+        roomName: '🟣⚫⚽️ 𝗦.𝗣.𝗟. 1 🏆 | Futsal 4v4 ⚽️⚫🟣',
+        playerName: 'host',
+        maxPlayers: 30,
+        public: false,
+        noPlayer: true,
+        token: 'thr1.AAAAAGW9P338BLvRrJ5OCQ.L46X3hVjHnA',
+        geo: {'code': 'gb', 'lat': 50, 'lon': 0},
+        roomScript: './pub.js'
+    },
+    publicdiv2: {
+        autoStart: true,
+        roomName: '🟣⚫⚽️ 𝗦.𝗣.𝗟. 2 🔥 | Futsal 4v4 ⚽️⚫🟣',
+        playerName: 'host',
+        maxPlayers: 30,
+        public: false,
+        noPlayer: true,
+        token: 'thr1.AAAAAGWYV2uVDc3nNi-SDg.P2QFF44FfRc',
+        geo: {'code': 'gb', 'lat': 50, 'lon': 0},
+        roomScript: './pub.js'
+    }
 };
 module.exports = config;
